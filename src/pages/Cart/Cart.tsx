b26d3da0ec8d791ffc27b78ebcb2436a9d83779b
edard3v <edard3v@gmail.com>
2025-01-03 05:10:58 -0500

    init store cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,10 @@
 import Layout from "@layouts/Layout/Layout";
 import { CART } from "./path";
+import { useCartStore } from "./useCartStore";
 
 export default function Cart() {
+  const products = useCartStore((store) => store.products);
+  console.log(products);
   return (
     <Layout title={CART.display}>
       <main>
